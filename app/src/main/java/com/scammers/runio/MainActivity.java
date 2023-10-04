9d898b3f86952fbc4b76e2420c3766b730ba25fe
srijan-goel19 <goelsrijan123@gmail.com>
2023-10-04 16:01:50 -0700

    Initial commit

---
+++ app/src/main/java/com/scammers/runio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.scammers.runio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

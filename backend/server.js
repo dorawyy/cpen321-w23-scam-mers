2815688c5dbc0b6623d01d53448c85c1b37af172
sgoel02 <goelsrijan123@gmail.com>
2023-10-04 22:16:11 -0700

    adding code for server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-// Add server code
+var express = require("express")
+var app = express()
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to RunIO")
+})
+
+var server = app.listen(8081, (req, res) => {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("RunIO Server Running on: http://%s:%s", host, port)
+})

886d08607ec66c0ae63c00c6f1b693db2b41a1ab
sgoel02 <goelsrijan123@gmail.com>
2023-10-07 23:51:06 -0700

    added db connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 var express = require("express")
 var app = express()
 
+const {MongoClient} = require("mongodb")
+const uri = "mongodb://localhost:27017"
+const client = new MongoClient(uri)
+
 app.get("/", (req,res)=>{
     res.send("Welcome to RunIO")
 })
 
-var server = app.listen(8081, (req, res) => {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("RunIO Server Running on: http://%s:%s", host, port)
-})
+async function run(){
+    try{
+        await client.connect()
+        console.log('Successfully Connected to MongoDB')
+        var server = app.listen(8081, (req, res) => {
+            var host = server.address().address
+            var port = server.address().port
+            console.log("RunIO Server Running on: http://%s:%s", host, port)
+        })
+    }
+    catch(err){
+        console.log(err)
+        await client.close()
+    }
+}
+
+run()

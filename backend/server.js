492dcad730fef8290e2f6be8c46bbf064f6be532
srijan-goel19 <goelsrijan123@gmail.com>
2023-10-16 15:53:21 -0700

    adding https for server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 var express = require("express")
 var app = express()
+const https = require('https')
+const fs = require('fs')
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
 
 const {MongoClient} = require("mongodb")
 const uri = "mongodb://localhost:27017"
@@ -13,11 +20,17 @@ async function run(){
     try{
         await client.connect()
         console.log('Successfully Connected to MongoDB')
-        var server = app.listen(8081, (req, res) => {
+        // var server = app.listen(8081, (req, res) => {
+        //     var host = server.address().address
+        //     var port = server.address().port
+        //     console.log("RunIO Server Running on: http://%s:%s", host, port)
+        // })
+        const server = https.createServer(options, app); // Use HTTPS server here
+        server.listen(443, () => { // Listen on the default HTTPS port (443)
             var host = server.address().address
             var port = server.address().port
-            console.log("RunIO Server Running on: http://%s:%s", host, port)
-        })
+            console.log("RunIO Server Running on: https://%s:%s", host, port)
+        });
     }
     catch(err){
         console.log(err)

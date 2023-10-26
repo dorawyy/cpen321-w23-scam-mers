3cae3f7348cd1e1724ae8ba8a15dbe9b5d2ae8a7
Ubuntu <azureuser@RunIOServer.l2ombkcmjdfebajr3qrrjdpsqc.xx.internal.cloudapp.net>
2023-10-26 01:44:19 +0000

    adding create user backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ var express = require("express")
 var app = express()
 const https = require('https')
 const fs = require('fs')
+const bodyParser = require('body-parser');
 
 const options = {
     key: fs.readFileSync('key.pem'),
@@ -9,35 +10,66 @@ const options = {
     passphrase: 'password'
 };
 
-const {MongoClient} = require("mongodb")
+const {MongoClient} = require("mongodb");
+const { emit } = require("process");
 const uri = "mongodb://localhost:27017"
 const client = new MongoClient(uri)
+
+// app.use(express.json()); // Add this line to enable JSON body parsing
+app.use(bodyParser.json());
 
 app.get("/", (req,res)=>{
     res.send("Welcome to RunIO")
 })
 
-app.post('/adduser', async (req, res) => {
+app.put('/user/:playerEmail', async (req, res) => {
   try {
-    const player = req.body;
+    const { playerEmail } = req.params;
+    const playerData = req.body;
+    // console.log("email:" + playerEmail);
+    // console.log("body:" + JSON.stringify(playerData));
 
-    if (!player) {
-      return res.status(400).json({ error: 'Player object is required' });
+    if (!playerEmail || !playerData) {
+      return res.status(400).json({ error: 'Insufficient player data fields' });
     }
 
-    const usersCollection = client.db("runio").collection("users");
+    const usersCollection = client.db("runio").collection("players");
+    const existingUser = await usersCollection.findOne({ playerEmail: playerEmail });
 
-    const result = await usersCollection.insertOne(player);
-
-    if (result.insertedCount === 1) {
-      return res.status(201).json(player);
+    if (existingUser) {
+      const result = await usersCollection.update({ _id: existingUser.id }, { $set: playerData });
+      return res.status(200).json({message: "Updated existing user"});
     } else {
-      return res.status(500).json({ error: 'User could not be added' });
+      const result = await usersCollection.insertOne(playerData)
+      return res.status(201).json({message: "Created new user"});
     }
   } catch (error) {
+    console.log("server error:" + error);
     return res.status(500).json({ error: 'Server error' });
   }
 });
+
+app.get('/user/:playerEmail', async (req, res) => {
+  try {
+    const { playerEmail } = req.params;
+
+    if (!playerEmail) {
+      return res.status(400).json({ error: 'Email is required' });
+    }
+    const usersCollection = client.db("runio").collection("players");
+    const existingUser = await usersCollection.findOne({ playerEmail: playerEmail });
+    if (existingUser) {
+      return res.status(200).json(existingUser);
+    } else {
+      return res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
 
 app.post('/tokensignin', async (req, res) => {
   const tokenId = req.body;

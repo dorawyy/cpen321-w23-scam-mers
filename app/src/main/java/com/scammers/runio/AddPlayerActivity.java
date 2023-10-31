51dc01f3e50d405710b9ada0eeae942dfe58308f
sgoel02 <goelsrijan123@gmail.com>
2023-10-31 10:03:31 -0700

    addPlayer working

--- app/src/main/java/com/scammers/runio/AddPlayerActivity.java
+++ app/src/main/java/com/scammers/runio/AddPlayerActivity.java
@@ -27,7 +27,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class AddPlayerActivity extends AppCompatActivity {
-    final static String TAG = "AddPlayer";
+    final static String TAG = "AddPlayerActivity";
     final static String ADD_PLAYER_URL = "https://40.90.192.159:8081/lobby/"; //TODO CHANGE THESE
 //    final static String PLAYER_ADD_LOBBY_URL = "https://40.90.192.159:8081/player/";
 
@@ -48,7 +48,7 @@ public class AddPlayerActivity extends AppCompatActivity {
             public void onClick(View view) {
                 invitedPlayerEmail = playerEmailInput.getText().toString();
                 //Lobby newLobby = new Lobby(invitedPlayerEmail, MainActivity.currentPlayer.getPlayerId());
-
+                Log.d(TAG, "invited player: " + invitedPlayerEmail);
                 // TODO: Send invitedPLayerEmail to backend, where it will be added to db
                 MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
 //                ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
@@ -66,11 +66,22 @@ public class AddPlayerActivity extends AppCompatActivity {
 
                     @Override
                     public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        Log.d(TAG, "RESPONSE CODE: " + response.code());
                         if(response.code() == 404){
                             // email not found in DB
-                            Toast.makeText(AddPlayerActivity.this, "E-Mail not registered: " + invitedPlayerEmail, Toast.LENGTH_LONG).show();
-                        } else if(response.code() == 200) {
+                            Log.d(TAG, "email not found");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    // Update UI elements here
+                                    Toast.makeText(AddPlayerActivity.this, "E-Mail not registered: " + invitedPlayerEmail, Toast.LENGTH_LONG).show();
+                                }
+                            });
+                            finish(); // maybe????
+                        }
+                        else if(response.code() == 200) {
                             String responseBody = response.body().string();
+                            Log.d(TAG, "Response player: " + responseBody);
                             try {
                                 Player invitedPlayer = new Player(new JSONObject(responseBody));
                                 Log.d(TAG, "MarcID: = " + invitedPlayer.getPlayerId());
@@ -81,8 +92,10 @@ public class AddPlayerActivity extends AppCompatActivity {
                                 // Retrieve the lobby ID from the intent's extras
                                 String lobbyId = getIntent().getStringExtra("lobbyIdAddPlayer");
                                 String putPlayerUrl = "https://40.90.192.159:8081/lobby/" + lobbyId + "/player/" + invitedPlayer.getPlayerId();
-
-                                RequestBody requestBody = RequestBody.create(invitedPlayer.toJSON(), mediaType);
+                                Log.d(TAG, "Player add url:" + putPlayerUrl);
+//                                RequestBody requestBody = RequestBody.create(invitedPlayer.toJSON(), mediaType);
+                                PlayerLobbyStats invitedPlayerStats = new PlayerLobbyStats();
+                                RequestBody requestBody = RequestBody.create(invitedPlayerStats.toJSON().toString(), mediaType);
                                 Request addPlayerReq = new Request.Builder()
                                         .url(putPlayerUrl)
                                         .put(requestBody)
@@ -97,8 +110,16 @@ public class AddPlayerActivity extends AppCompatActivity {
                                     public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                                         if (response.isSuccessful()) {
                                             // Handle the successful response here
+                                            // also make sure to toast if player was already added
                                             //player was added :)
                                             Log.d(TAG, "Player added");
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    // Update UI elements here
+                                                    Toast.makeText(AddPlayerActivity.this, "Added player: " + invitedPlayerEmail, Toast.LENGTH_LONG).show();
+                                                }
+                                            });
                                         } else {
                                             // Handle the error response here
                                             Log.d(TAG, "Error inviting player: " + response);
@@ -108,13 +129,12 @@ public class AddPlayerActivity extends AppCompatActivity {
                             } catch (JSONException e) {
                                 throw new RuntimeException(e);
                             }
-                        } else {
+                        }
+                        else {
                             throw new IOException("Unexpected code " + response);
                         }
                     }
                 });
-
-                Toast.makeText(AddPlayerActivity.this, "Added player: " + invitedPlayerEmail, Toast.LENGTH_LONG).show();
 
                 // Close add player activity once finished
                 finish();
